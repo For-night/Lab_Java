6c6940b82c0b5df17cbdc51bb5f52e0bb5d3c2bb
yqdvan <35555798+yqdvan@users.noreply.github.com>
2020-12-02 22:01:38 +0800

    Update The nearest common ancestor of binary trees.java

--- The nearest common ancestor of binary trees.java
+++ The nearest common ancestor of binary trees.java
@@ -10,6 +10,7 @@
 
  //遇到问题永远都是先分析问题！
  //回溯中会丢弃很多个节点，因为潜在叶节点很多，而我们只要目标的两个或者一个节点
+// *****这个是后序遍历！ 吗？******//
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
@@ -22,4 +23,24 @@ class Solution {
         if(left == null && right == null) return right;
         return left == null ? right : left;
     }
+}
+
+
+// ***非递归版本先序遍历*** //
+public static void preOrder(TreeNode biTree){
+ //非递归实现
+	Stack<TreeNode> stack = new Stack<TreeNode>();
+	while(biTree != null || !stack.isEmpty())	{
+  
+		while(biTree != null)		{
+			System.out.println(biTree.value);
+			stack.push(biTree);
+				biTree = biTree.left;
+		}
+		if(!stack.isEmpty())		{
+			biTree = stack.pop();
+			biTree = biTree.right;
+		}
+  
+	}
 }
