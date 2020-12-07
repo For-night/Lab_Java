96925975cb5438764fcacbfbcf1a5f4d332a5b3e
yqdvan <35555798+yqdvan@users.noreply.github.com>
2020-12-07 21:52:01 +0800

    Update The nearest common ancestor of binary search tree.java

--- The nearest common ancestor of binary search tree.java
+++ The nearest common ancestor of binary search tree.java
@@ -1,16 +1,26 @@
 class Solution {
-    public TreeNode v(TreeNode root, TreeNode p, TreeNode q) {
-        
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(p.val < q.val){
+            return f(root,p,q);
+        }else{
+            return f(root,q,p);
+        }
+
+    }
+
+    public static TreeNode f(TreeNode root, TreeNode p, TreeNode q){
+        if(p == q){
+            return q;
+        }
+
         if( p.val <= root.val && root.val <= q.val){
             return root;
-        }
+        } 
 
         if(root.val < p.val){
-            return lowestCommonAncestor(root.left , p , q);
-        }
-
-        if(root.val > q.val){
-            return lowestCommonAncestor(root.right , p , q);
+            return f(root.right , p , q);
+        }else{
+            return f(root.left , p , q);
         }
     }
 }
