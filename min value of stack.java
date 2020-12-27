4cc4df1fc88127f36bc4bb148b12f539b0a92cef
yqdvan <35555798+yqdvan@users.noreply.github.com>
2020-12-27 17:38:45 +0000

    Create min value of stack.java

---
+++ min value of stack.java
@@ -0,0 +1,33 @@
+class MinStack {
+    static Stack<Integer> s , help;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        s = new Stack<Integer>();
+        help = new Stack<Integer>();
+    }
+    
+    public void push(int x) {
+        s.push(x);
+        if(help.isEmpty()){
+            help.push(x);
+        }else if(help.peek() >= x){
+            help.push(x);
+        }
+    }
+    
+    public void pop() {
+        int ache = s.pop();
+        if(ache == help.peek()){
+            help.pop();
+        }
+    }
+    
+    public int top() {
+        return s.peek();
+    }
+    
+    public int min() {
+        return help.peek();
+    }
+}
